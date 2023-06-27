90ab3fe72a9f11fbe815287ce7e89c2ae6f7e61f
Dilip Patel <dilip.dekavadiya@bp.liferay.com>
2023-06-27 17:47:10 +0530

    first commit

---
+++ js/custome.js
@@ -0,0 +1,181 @@
+$('.innovators-slider-con').slick({
+  slidesToShow: 3,
+
+  slidesToScroll: 1,
+
+  autoplay: true,
+
+  autoplaySpeed: 6000,
+
+  arrows: true,
+
+  dots: false,
+
+  speed: 3000,
+
+  responsive: [
+    {
+      breakpoint: 1200,
+
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+
+    {
+      breakpoint: 992,
+
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+
+    {
+      breakpoint: 768,
+
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+
+    {
+      breakpoint: 576,
+
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
+
+$('.community-life-slider-con').slick({
+  slidesToShow: 3,
+
+  slidesToScroll: 1,
+
+  autoplay: true,
+
+  autoplaySpeed: 5000,
+
+  arrows: true,
+
+  dots: false,
+
+  speed: 3000,
+
+  responsive: [
+    {
+      breakpoint: 1200,
+
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+
+    {
+      breakpoint: 992,
+
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+
+    {
+      breakpoint: 768,
+
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+
+    {
+      breakpoint: 576,
+
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
+
+$('.logo-slider').slick({
+  slidesToShow: 5,
+
+  slidesToScroll: 1,
+
+  autoplay: true,
+
+  autoplaySpeed: 2000,
+
+  arrows: true,
+
+  dots: false,
+
+  speed: 3000,
+
+  responsive: [
+    {
+      breakpoint: 1200,
+
+      settings: {
+        slidesToShow: 5,
+      },
+    },
+
+    {
+      breakpoint: 992,
+
+      settings: {
+        slidesToShow: 4,
+      },
+    },
+
+    {
+      breakpoint: 768,
+
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+
+    {
+      breakpoint: 576,
+
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+  ],
+});
+
+$('.testimonials-slider').slick({
+  slidesToShow: 1,
+
+  slidesToScroll: 1,
+
+  autoplay: true,
+
+  autoplaySpeed: 6000,
+
+  arrows: true,
+
+  dots: false,
+
+  speed: 3000,
+});
+
+$('.single-img-slider').slick({
+  slidesToShow: 1,
+
+  slidesToScroll: 1,
+
+  autoplay: true,
+
+  autoplaySpeed: 2000,
+
+  arrows: true,
+
+  dots: false,
+
+  speed: 1000,
+});
